b5d3050c1533641b1d642d22ed9a823b9bfcaf17
Brunno Lorran <brunnolorran.silva@gmail.com>
2020-12-09 21:04:27 +0000

    Revert "Merge branch 'feature/login' into 'master'"
    This reverts merge request !6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import GlobalStyle from "./styles/global";
-import RegistrationForm from "./components/userRegister";
-import Login from "./pages/login";
-
-//RegistrationForm inserida apenas para teste. Substituir alert por modal alert.
 
 const App = () => {
   return (
     <>
       <div id="main-container">
         <h1>KenzieHub</h1>
-        <RegistrationForm />
-        <Login />
       </div>
       <GlobalStyle></GlobalStyle>
     </>
--- src/store/modules/usersBasics/actions.js
+++
@@ -1,3 +0,0 @@
-import { USER_BASICS } from "./types";
-
-export const showUsers = (list) => ({ type: USER_BASICS, list });
