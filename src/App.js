3873e31d5d8dfcb758f80ff3ae992a33781c7f5b
Wenner Castilho <wennercastilho@gmail.com>
2020-12-09 16:17:51 -0300

    feature/login: Login page integrated in the pages folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import GlobalStyle from "./styles/global";
-import RegistrationForm from "./components/userRegister"
+import RegistrationForm from "./components/userRegister";
+import Login from "./pages/login";
 
 //RegistrationForm inserida apenas para teste. Substituir alert por modal alert.
-
 
 const App = () => {
   return (
     <>
       <div id="main-container">
         <h1>KenzieHub</h1>
-        <RegistrationForm/> 
+        <RegistrationForm />
+        <Login />
       </div>
       <GlobalStyle></GlobalStyle>
     </>
