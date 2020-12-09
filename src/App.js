84be071f167ab838a5b534dbf3c3f8fdac643a37
Wenner Castilho <wennercastilho@gmail.com>
2020-12-09 17:44:26 -0300

    feature/routes: App.js file with all routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import GlobalStyle from "./styles/global";
-import RegistrationForm from "./components/userRegister"
+import RegistrationForm from "./components/userRegister";
+import Profile from "./pages/perfil";
+// import Login from "./pages/login";
+
+import { Switch, Route } from "react-router-dom";
 
 //RegistrationForm inserida apenas para teste. Substituir alert por modal alert.
-
 
 const App = () => {
   return (
     <>
       <div id="main-container">
         <h1>KenzieHub</h1>
-        <RegistrationForm/> 
+        <Switch>
+          <Route exact path="/">
+            Landing Page Home
+          </Route>
+          <Route path="/login">
+            Login
+            {/* <Login /> */}
+          </Route>
+          <Route path="/register">
+            <RegistrationForm />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/home">Home</Route>
+          <Route path="/profile-users">Profile Users</Route>
+        </Switch>
       </div>
       <GlobalStyle></GlobalStyle>
     </>
