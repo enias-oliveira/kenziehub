60d388f304ccb4ba93b75a94e757f838b3ccab06
Enias Oliveira <eniasoliveira27@gmail.com>
2020-12-15 17:10:16 -0300

    feat: makes pagination size dynamic

--- src/components/listUserHome/index.jsx
+++ src/components/listUserHome/index.jsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
 import { List } from "antd";
 import CardUserHome from "../../components/cardUserHome";
 
+import axios from "axios";
+
 const ListUserHome = ({ users, currentPage, setPage }) => {
+  const [totalUsers, setTotalUsers] = useState(0);
+
+  const getTotalUsers = () => {
+    axios
+      .get("https://kenziehub.me/users?perPage=1000000&page=1")
+      .then((response) => setTotalUsers(response.data.length));
+  };
+
+  useEffect(() => {
+    getTotalUsers();
+  });
+
   return (
     <>
       <List
@@ -19,8 +34,8 @@ const ListUserHome = ({ users, currentPage, setPage }) => {
           onChange: (page) => setPage(page),
           pageSize: 12,
           current: currentPage,
-          total: 300,
-          background: "blue",
+          total: totalUsers,
+          showSizeChanger: false,
         }}
         dataSource={users}
         renderItem={(user) => (
